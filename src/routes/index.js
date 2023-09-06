70a8ffa19b79e37727734aebe1d82a1ff529b311
joffinjoy <joffinjoy@gmail.com>
2023-09-06 21:29:48 +0530

    Formatting Changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,7 @@
 'use strict'
 const router = require('express').Router()
 const catalogController = require('@controllers/')
-/* const { authVerifier } = require('@middlewares/authVerifier')
 
-router.use(authVerifier) */
-
-console.log("Navigation is happening")
 router.post('/index-session', catalogController.search)
 router.post('/get-sessions/', catalogController.getFulfillment)
 router.get('/get-session/:sessionId', catalogController.getSession)
--- src/services/search.js
+++ src/services/search.js
@@ -7,24 +7,24 @@
 // const { fulfillmentObjectDTO } = require('@dtos/fulfillmentObject')
 // const { getCount, getDocs, getSourceObject, flattenArrayOfArrays } = require('@utils/generic')
 
-const { indexDocument,  getDocumentById, searchDocuments} = require('@utils/elasticsearch')
+const { indexDocument, getDocumentById, searchDocuments } = require('@utils/elasticsearch')
 
 const searchByPersonName = async (requestBody) => {
-    console.log("here is body", requestBody)
+	console.log('here is body', requestBody)
 }
 
 exports.indexSessions = async (requestBody) => {
-	console.log("Hello World")
+	console.log('Hello World')
 }
 
 exports.getFulfillment = async (fulfillmentId) => {
-    console.log("Hello World 2")
+	console.log('Hello World 2')
 }
 
 exports.getSessioById = async (sessionId, getAllProtocolObjects) => {
-    console.log("Hello World 3")
+	console.log('Hello World 3')
 }
 
 exports.getStatusBody = async (sessionId, fulfillmentId) => {
-    console.log("Hello WOrld 4")
+	console.log('Hello WOrld 4')
 }
